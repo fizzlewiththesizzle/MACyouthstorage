a248f06d05aa192df4c3bc43fffcdf67676a6158
moahmed2104 <moahmed2104@gmail.com>
2021-08-31 14:42:56 -0600

    no sheets >:(

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, redirect, url_for, render_template, request
+from sheets import *
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 app = Flask(__name__)
+
+SERVICE_ACCOUNT_FILE = 'keys.json'
+SCOPES= ['https://www.googleapis.com/auth/spreadsheets']
+
+creds= None
+creds= service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# The ID and range of a sample spreadsheet.
+sheetID= '1-BUCK6upv_LehdOwkKuaK461VF54Cq2RP-lvPZIl4gQ'
+
+service= build('sheets', 'v4', credentials=creds)
 
 @app.route("/")  # this sets the route to this page
 def home():
@@ -10,16 +25,19 @@ def home():
 def add():
 
     if request.method == "POST":
+        equipmentName = request.form.get("equipmentName")
+        equipmentQuantity = request.form.get("equipmentQuantity")
 
-        req= request.form
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=sheetID, range="Sheet1!A2:B500").execute()
+        values = result.get('values', [])
 
-        equipmentName= req["equipmentName"]
-        equipmentQuantity= req["equipmentQuantity"]
-
-        print(equipmentName, equipmentQuantity)
+        data = [[equipmentName, equipmentQuantity]] #array of arrays, could make data equals textfield input for frontend
+        res = service.spreadsheets().values().append(spreadsheetId=sheetID, range="Sheet1!A1:B1", valueInputOption="USER_ENTERED", 
+                                                                insertDataOption="INSERT_ROWS", body={"values":data}).execute()
+        print(res)
 
         return redirect(request.url)
-
     return render_template("add.html")
 
 @app.route("/signout", methods= ["GET", "POST"])
@@ -27,11 +45,9 @@ def signout():
 
     if request.method == "POST": 
 
-        req= request.form
-
-        userName= req["userName"]
-        equipmentName= req["equipmentName"]
-        equipmentQuantity= req["equipmentQuantity"]
+        userName = request.form.get("userName")
+        equipmentName = request.form.get("equipmentName")
+        equipmentQuantity= request.form.get("equipmentQuantity")
 
         print(userName, equipmentName, equipmentQuantity)
 
@@ -41,3 +57,6 @@ def signout():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+#SHeets
+
